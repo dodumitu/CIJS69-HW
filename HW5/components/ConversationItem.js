4964c469efed1ae87c6c13588b9b1886d2b755a7
Đỗ Duy Minh Tuấn <80260231+dodumitu@users.noreply.github.com>
2021-11-30 10:42:54 +0700

    update

--- HW5/components/ConversationItem.js
+++ HW5/components/ConversationItem.js
@@ -4,10 +4,11 @@ export default class ConversationItem {
   $nameEl;
   constructor(){
     this.$container = document.createElement('div')
-    this.$container.setAttribute('class', 'flex bg-red-200 rounded-full');
+    this.$container.setAttribute('class', 'flex bg-red-200 mt-4 rounded-full');
     this.$proPicEl = document.createElement('img')
     this.$proPicEl.setAttribute('src','https://tapchianhdep.com/wp-content/uploads/2021/05/anh-avatar-cho-con-gai-cuc-dep.jpg')
     this.$proPicEl.setAttribute('class', "h-12 w-12 self-center rounded-full ")
+    this.$nameEl
   }
   render(conversationsContainer){
     // const test = document.createElement('h2');
