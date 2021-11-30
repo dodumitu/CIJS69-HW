60f85ee03c53306d51a2e0ffb8a367653b1cf713
Đỗ Duy Minh Tuấn <80260231+dodumitu@users.noreply.github.com>
2021-11-30 10:39:47 +0700

    update

--- HW5/components/ConversationList.js
+++ HW5/components/ConversationList.js
@@ -24,7 +24,6 @@ export default class ConversationList {
       "class",
       "w-1/4 h-full py-8 px-4 bg-blue-400"
     );
-    // conversationsListContainer.textContent = "conversation Container";
     conversationsListContainer.appendChild(this.$newConversationButton)
     conversationsListContainer.app;
     this.$createConversationModal.render(conversationsListContainer)
@@ -33,40 +32,3 @@ export default class ConversationList {
     mainContainer.appendChild(conversationsListContainer);
   }
 }
-// import ConversationItem from "./ConversationItem.js";
-// import CreateNewConversationModal from "./CreateNewConversationModal.js";
-
-// export default class ConversationList {
-//   $conversationItem;
-//   $newConversationButton;
-//   $createConversationModal;
-
-//   constructor() {
-//     this.$conversationItem = new ConversationItem();
-//     this.$createConversationModal = new CreateNewConversationModal();
-
-//     this.$newConversationButton = document.createElement("button");
-//     this.$newConversationButton.textContent = "New conversation";
-//     this.$newConversationButton.setAttribute(
-//       "class",
-//       "bg-white py-2 px-4 font-bold rounded-full"
-//     );
-//     this.$newConversationButton.addEventListener("click", () => {
-//       this.$createConversationModal.opentModal();
-//     });
-//   }
-
-//   render(mainContainer) {
-//     const conversationListContainer = document.createElement("div");
-//     conversationListContainer.setAttribute(
-//       "class",
-//       "w-1/4 h-full py-4 px-8 bg-blue-400"
-//     );
-//     conversationListContainer.appendChild(this.$newConversationButton);
-//     conversationListContainer.app;
-//     this.$createConversationModal.render(conversationListContainer);
-
-//     this.$conversationItem.render(conversationListContainer);
-//     mainContainer.appendChild(conversationListContainer);
-//   }
-// }
