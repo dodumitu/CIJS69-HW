46f612adfc5453ad78452c04e8ae6804b26e8f79
Đỗ Duy Minh Tuấn <80260231+dodumitu@users.noreply.github.com>
2021-12-10 09:15:06 +0700

    update

--- HW5 ++/components/InputGroup.js
+++ HW5 ++/components/InputGroup.js
@@ -1,16 +1,16 @@
 class InputGroup {
     $containerEl;
-    $labelEl;
+    $lableEl;
     $inputEl;
     $errorEl;
   
-    constructor(type, labelName, placeholder) {
+    constructor(type, lableName, placeholder) {
       this.$containerEl = document.createElement("div");
-      this.$containerEl.setAttribute("class", "flex flex-col m-4");
+      this.$containerEl.setAttribute("class", "flex flex-col mb-4");
   
-      this.$labelEl = document.createElement("label");
-      this.$labelEl.textContent = labelName;
-      this.$labelEl.setAttribute("class", "text-white font-bold mb-2");
+      this.$lableEl = document.createElement("label");
+      this.$lableEl.textContent = lableName;
+      this.$lableEl.setAttribute("class", "text-white font-bold mb-2");
   
       this.$inputEl = document.createElement("input");
       this.$inputEl.type = type;
@@ -27,7 +27,7 @@ class InputGroup {
     setError(message) {
       if (message !== "") {
         this.$errorEl.textContent = message;
-        this.$errorEl.setAttribute("class", "text-red-700 text-md");
+        this.$errorEl.setAttribute("class", "text-red-700 text-md font-bold");
       } else {
         this._errorValue = "";
         this.$errorEl.textContent = "";
@@ -35,7 +35,7 @@ class InputGroup {
     }
   
     render() {
-      this.$containerEl.appendChild(this.$labelEl);
+      this.$containerEl.appendChild(this.$lableEl);
       this.$containerEl.appendChild(this.$inputEl);
       this.$containerEl.appendChild(this.$errorEl);
   
